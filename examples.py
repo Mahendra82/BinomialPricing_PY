4783c3a4fe84ff9d425868238bfd683eadd6f173
Mahendra82 <getmahendra@gmail.com>
2022-09-13 01:22:58 +0530

    Draft changes to check for NSE
    Draft changes to check for NSE

--- examples.py
+++ examples.py
@@ -1,5 +1,6 @@
 from eu_option import euro_option
 
+'need to change to check for Indian stocks!
 option_eu = euro_option(217.58, 215, 0.05, 0.1, 40, {'tk': 'AAPL', 'is_calc': True, 'start': '2017-08-18',
                                                      'end': '2018-08-18', 'eu_option':False})
 option_eu2 = euro_option(217.58, 215, 0.05, 0.1, 40, {'tk': 'AAPL', 'is_calc': True, 'use_garch': True, 'start': '2017-08-18',
